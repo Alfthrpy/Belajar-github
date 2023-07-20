427e083ca8cbfdb0ba115ff934a71bdc8a2cc6cd
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2023-07-20 08:51:38 +0700

    menambah fitur iterasi kalimat

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print("Ini adalah program untuk menampilkan angka dari 1 - 10 dengan loop di pyt
 
 for i in range(1,11):
   print(i,end="")
+
+print("ini adalah program untuk meiterasi suatu kalimat yang diinput oleh user")
+
+str = input("masukan kalimat : ")
+for i in str.split():
+  print(i)
+
