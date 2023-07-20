176a07cab5574adaefa83aa238e4f90094c13c02
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2023-07-20 08:52:24 +0700

    menghapus fitur menampilkan angka 1 - 10

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 print("Hallo, selamat datang di program kami")
-print("Ini adalah program untuk menampilkan angka dari 1 - 10 dengan loop di python")
-
-for i in range(1,11):
-  print(i,end="")
-
 print("ini adalah program untuk meiterasi suatu kalimat yang diinput oleh user")
 
 str = input("masukan kalimat : ")
